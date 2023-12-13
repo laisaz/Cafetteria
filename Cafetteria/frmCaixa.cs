54de8934f0d8d089f91c1c55f59c765aa581a4a1
lugranatof <luiza.granato11@gmail.com>
2023-12-13 08:18:00 -0300

    a1

--- Cafetteria/frmCaixa.cs
+++ Cafetteria/frmCaixa.cs
@@ -48,5 +48,10 @@ namespace Cafetteria
                 Application.Exit();
             }
         }
+
+        private void btnCancela_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
